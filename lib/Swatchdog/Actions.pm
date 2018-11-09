7812e7abd0a4af000c0f81c88b0ebba8861085b6
Tom Callaway <spot@fedoraproject.org>
2018-11-09 09:09:53 -0500

    using the exec command in a swatch config file causes zombie processes to be left

--- lib/Swatchdog/Actions.pm
+++ lib/Swatchdog/Actions.pm
@@ -96,7 +96,7 @@ sub exec_command {
 
  EXECFORK: {
     if ($exec_pid = fork) {
-      waitpid(-1, WNOHANG);
+      waitpid($exec_pid, 0);
       return 0;
     } elsif (defined $exec_pid) {
       exec($command);
